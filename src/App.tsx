c1f5de5f121bae482550e97715a1c7a81bb93f23
Aziz Saidi <saidi11aziz2004@gmail.com>
2024-12-24 00:56:23 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './Layoutss/header';
 import { Acceuil } from './AllPages/Accueil';
 import { Adoption } from './AllPages/Adoptions';
